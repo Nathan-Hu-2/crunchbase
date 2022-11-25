78200239801f1fc2061b810556f2584e54060c49
Nathan Hu <nathanhu0321@gmail.com>
2022-11-25 05:52:51 +0100

    scrape tech data as well

--- crunchbase.py
+++ crunchbase.py
@@ -10,23 +10,42 @@
 
 import requests
 from bs4 import BeautifulSoup
+from csv import writer
 
 
-url = 'https://www.crunchbase.com/organization/pipe'
+base_url = 'https://www.crunchbase.com/organization/pipe'
+technology_url = 'https://www.crunchbase.com/organization/pipe/technology'
+
 headers = {'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
 
-res = requests.get(url, headers=headers)
-# print(res.text)
+res = requests.get(base_url, headers=headers)
 
-soup = BeautifulSoup(res.text, 'html.parser')
+# Base Organisation information
+soup_base = BeautifulSoup(res.text, 'html.parser')
 
-company_info = {
-    'company_name': soup.find('h1', class_="profile-name").text,
-    'company_description': soup.find('span', class_="description").text,
-    'total_funding': soup.find('span', class_="component--field-formatter field-type-money ng-star-inserted").text
-}
+res = requests.get(technology_url, headers=headers)
+soup_tech = BeautifulSoup(res.text, 'html.parser')
 
-print(company_info)
+
+# print(company_info)
+
+with open('crunchbase.csv', 'w', encoding='utf8', newline='') as f:
+    theWriter = writer(f)
+    header = ['Company Name', 'Description', 'Funding', 'Founded', 'Monthly Site Visits', 'Monthly Visits Growth']
+
+    theWriter.writerow(header)
+
+    company_name = soup_base.find('h1', class_="profile-name").text
+    company_description = soup_base.find('span', class_="description").text
+    total_funding = soup_base.find('span', class_="component--field-formatter field-type-money ng-star-inserted").text
+    founded = soup_base.find('span', class_="component--field-formatter field-type-date_precision ng-star-inserted").text
+
+    monthly_site_visits = soup_tech.find_all('span', class_="component--field-formatter field-type-integer ng-star-inserted")
+
+
+    info = [company_name, company_description, total_funding, founded, monthly_site_visits]
+    print(info)
+    theWriter.writerow(info)
 
 
 # print("Company Name:", company_name)
