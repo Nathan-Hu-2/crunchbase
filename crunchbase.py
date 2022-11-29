6f24ec06c5f94510825d13270c5a9423979e444d
Nathan Hu <nathanhu0321@gmail.com>
2022-11-29 03:36:32 +0100

    added code for rotating ip

--- crunchbase.py
+++ crunchbase.py
@@ -6,7 +6,6 @@
 # print(res.content)
 # print(res.text)
 
-
 import requests
 from bs4 import BeautifulSoup
 from csv import writer
@@ -15,33 +14,35 @@ import random
 import re
 import sys
 import time
-from torpy.http.requests import TorRequests
+# from torpy.http.requests import TorRequests
+from fake_useragent import UserAgent
+
+from stem import Signal
+from stem.control import Controller
+def switchIP():
+    with Controller.from_port(port = 3000) as controller:
+        controller.authenticate()
+        controller.signal(Signal.NEWNYM)
 
 start = time.time()
 
 def delay() -> None:
-    time.sleep(random.uniform(3, 7))
+    time.sleep(random.uniform(0.100, 0.300))
     return None
 
 companies = ['pipe']
 
 for company in companies:
+    # switchIP()
     base_url = f'https://www.crunchbase.com/organization/{company}'
     technology_url = f'https://www.crunchbase.com/organization/{company}/technology'
     signals_url = f'https://www.crunchbase.com/organization/{company}/signals_and_news'
 
-    headers = {'User-Agent': "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
-
-    # Base Organisation Information
-    # with TorRequests() as tor_requests:
-    #     with tor_requests.get_session() as sess:
-    #         res = sess.get(base_url) # fire request
-    #         print(res.status_code)
-    #         soup_base = BeautifulSoup(res.text, 'html.parser')
+    headers = {'User-Agent': UserAgent().random, "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
 
     res = requests.get(base_url, headers=headers)
     soup_base = BeautifulSoup(res.text, 'html.parser')
-    print(soup_base)
+    print(res.status_code)
 
     # Grab Tech Related Information
     res = requests.get(technology_url, headers=headers)
@@ -74,8 +75,8 @@ for company in companies:
         num_articles = soup_signal.find('a', class_="component--field-formatter field-type-integer link-accent ng-star-inserted").text
 
         info = [company_name, company_description, total_funding, founded, monthly_site_visits, monthly_visits_growth, num_articles]
-        print(info)
         theWriter.writerow(info)
+        switchIP()
 
 end = time.time()
 print(end - start)
